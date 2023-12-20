2dc5049929ca31629682ef77edd8bcb4cc03f405
Роман Кузора <romakuzora@mail.ru>
2023-12-20 17:47:55 +0300

    доработка базы и основы всея игры

--- main.py
+++ main.py
@@ -21,61 +21,54 @@ class Board:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.board = [[0] * width for _ in range(height)]
+        self.board = [[None] * width for _ in range(height)]
         self.cell_size = 50
 
-    def set_view(self, left, top, cell_size):
-        self.left = left
-        self.top = top
+    def set_view(self, cell_size):
         self.cell_size = cell_size
 
     def render(self, screen):
-        for y in range(self.height):
-            for x in range(self.width):
-                if self.board[y][x] == 0:
-                    color = pygame.Color('black')
-                elif self.board[y][x] == 1:
-                    color = pygame.Color('blue')
-                pygame.draw.rect(screen, color,
-                                 (x * self.cell_size, y * self.cell_size, self.cell_size,
-                                  self.cell_size))
-                pygame.draw.rect(screen, pygame.Color('white'),
-                                 (x * self.cell_size, y * self.cell_size, self.cell_size,
-                                  self.cell_size), width=1)
-
-    def make_ice(self):
-        for y in range(self.height):
-            for x in range(self.width):
-                if 0 <= x <= 1 or 0 <= y <= 1:
-                    self.board[y][x] = 1
-                if self.width - 2 <= x <= self.width - 1 or self.height - 2 <= y <= self.height - 1:
-                    self.board[y][x] = 1
+        for x in range(len(self.board)):
+            for y in range(len(self.board[x])):
+                if self.board[x][y] is None:
+                    pygame.draw.rect(screen, (255, 255, 255),
+                                     (x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size), width=1)
+                elif self.board[x][y].name == 'hero':
+                    self.board[x][y].rect.x, self.board[x][
+                        y].rect.y = x * self.cell_size, y * self.cell_size
 
 
 class Unit(pygame.sprite.Sprite):
-    def init(self):
-        super().__init__()
-        self.image = load_image("IceCream.png")
+    def init(self, name_person, name_sprite, x, y):
+        super().__init__(all_sprites)
+        self.image = load_image(name_sprite)
         self.rect = sprite_hero.image.get_rect()
-        self.rect.x, self.rect.y = 130, 130
+        self.x, self.y = x, y
+        self.name = name_person
 
-    def go(self, board, direction):
-        # эта функция для проверки того может ли герой пойти в клетку или нет вдруг в той клетке лед
-        value = (0, 0)
-        if direction == 'left':
-            value = (-25, 0)
-        elif direction == 'right':
-            value = (25, 0)
-        elif direction == 'up':
-            value = (0, 25)
-        elif direction == 'down':
-            value = (0, -25)
-        # создание значения на которое будме менять координаты х и у
-        x = (self.rect.x + 30) // board.cell_size
-        y = (self.rect.y + 30) // board.cell_size
-        # координаты клетки в которой герой
-        self.rect.x += value[0]
-        self.rect.y += value[1]
+    def go_down(self, board):
+        if self.y + 1 < board.height:
+            board.board[self.x][self.y + 1] = board.board[self.x][self.y]
+            board.board[self.x][self.y] = None
+            self.y += 1
+
+    def go_up(self, board):
+        if self.y - 1 >= 0:
+            board.board[self.x][self.y - 1] = board.board[self.x][self.y]
+            board.board[self.x][self.y] = None
+            self.y -= 1
+
+    def go_left(self, board):
+        if self.x - 1 >= 0:
+            board.board[self.x - 1][self.y] = board.board[self.x][self.y]
+            board.board[self.x][self.y] = None
+            self.x -= 1
+
+    def go_right(self, board):
+        if self.x + 1 < board.width:
+            board.board[self.x + 1][self.y] = board.board[self.x][self.y]
+            board.board[self.x][self.y] = None
+            self.x += 1
 
 
 if __name__ == '__main__':
@@ -84,14 +77,13 @@ if __name__ == '__main__':
     size = wight, height = 800, 800
     screen = pygame.display.set_mode(size)
     board = Board(16, 16)
-    pygame.display.set_caption('Bad Icecream')
+    pygame.display.set_caption('Фруктозавр')
     running = True
 
     all_sprites = pygame.sprite.Group()
     sprite_hero = Unit()
-    sprite_hero.init()
-    all_sprites.add(sprite_hero)
-    # спрайт героя
+    board.board[0][0] = sprite_hero
+    sprite_hero.init('hero', 'shag_levo.png', 0, 0)
 
     clock = pygame.time.Clock()
     while running:
@@ -99,19 +91,17 @@ if __name__ == '__main__':
             keys = pygame.key.get_pressed()
             if event.type == pygame.QUIT:
                 running = False
+            if keys[115]:
+                sprite_hero.go_down(board)
+            if keys[119]:
+                sprite_hero.go_up(board)
+            if keys[97]:
+                sprite_hero.go_left(board)
+            if keys[100]:
+                sprite_hero.go_right(board)
 
-            elif keys[115]:
-                sprite_hero.go(board, 'up')
-            elif keys[119]:
-                sprite_hero.go(board, 'down')
-            elif keys[97]:
-                sprite_hero.go(board, 'left')
-            elif keys[100]:
-                sprite_hero.go(board, 'right')
-            # проверка на движение
         clock.tick(30)
         screen.fill((0, 0, 0))
-        board.make_ice()
         board.render(screen)
         all_sprites.draw(screen)
         pygame.display.flip()
