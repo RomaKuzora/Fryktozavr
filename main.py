bf8d25c4633f0a64398f3db20e25dfebe38fdb8e
Qual1tyGoose <lebedinsergey2017@gmail.com>
2023-12-29 14:22:24 +0300

    Покадровый спавн льда, v4(окончательный), также исправлено несколько ошибок PEP 8

--- main.py
+++ main.py
@@ -3,26 +3,26 @@ import os
 import pygame
 
 
-def spawn_ice(move):  # перенес функцию т.к. она созадвала экземпляры класса в котором
+def spawn_ice(last_move):  # перенес функцию т.к. она созадвала экземпляры класса в котором
     # находилась и добавляла в левый список так не надо делать
     # у нас есть ice_sprites это группа спрайтов льда и еще добавил чтобы на доске клетка менялась с None на 'ice'
     # чтобы проверять потом по клеткам куда можно идти но что-то пошло не по плану и делайте дальше сами короче
-    x = sprite_hero.rect.x // board.cell_size  # смотрим на какой клетке стоял дино
-    y = sprite_hero.rect.y // board.cell_size
-    if move[0] == 1:  # определяем куда смотрел дино последний раз
-        for i in range(x + shagg + 1, 20):
-            ice_list.append((y, i))  # запоминаем на каих координатах ставим  лёд
-    elif move[0] == -1:
-        for i in range(x - 1, -1, -1):
-            ice_list.append((y, i))
+    xx = sprite_hero.rect.x // board.cell_size  # смотрим на какой клетке стоял дино
+    yy = sprite_hero.rect.y // board.cell_size
+    if last_move[0] == 1:  # определяем куда смотрел дино последний раз
+        for i in range(xx + shagg + 1, 20):
+            ice_list.append((yy, i))  # запоминаем на каих координатах ставим  лёд
+    elif last_move[0] == -1:
+        for i in range(xx - 1, -1, -1):
+            ice_list.append((yy, i))
 
-    elif move[1] == -1:
-        for i in range(y - 1, -1, -1):
-            ice_list.append((i, x))
+    elif last_move[1] == -1:
+        for i in range(yy - 1, -1, -1):
+            ice_list.append((i, xx))
 
-    elif move[1] == 1:
-        for i in range(y + shagg + 1, 12):
-            ice_list.append((i, x))
+    elif last_move[1] == 1:
+        for i in range(yy + shagg + 1, 12):
+            ice_list.append((i, xx))
 
 
 def possition(mouse_pos):
@@ -51,22 +51,22 @@ colorkey = (255, 255, 255)
 
 
 class Board:
-    def __init__(self, width, height):
+    def __init__(self, width, heightt):
         self.width = width
-        self.height = height
-        self.board = [[None] * width for _ in range(height)]
+        self.height = heightt
+        self.board = [[None] * width for _ in range(heightt)]
         self.cell_size = 68
 
-    def set_view(self, cell_size):
-        self.cell_size = cell_size
+    def set_view(self, cell_cize):
+        self.cell_size = cell_cize
 
-    def render(self, screen):
-        for y in range(len(self.board)):
-            for x in range(len(self.board[y])):
-                if self.board[y][x] is None:
-                    pygame.draw.rect(screen, (255, 255, 255),
-                                     (x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size), width=1)
-
+    def render(self, screen_1):
+        for yy in range(len(self.board)):
+            for xx in range(len(self.board[y])):
+                if self.board[yy][xx] is None:
+                    pygame.draw.rect(screen_1, (255, 255, 255),
+                                     (xx * self.cell_size, yy * self.cell_size, self.cell_size, self.cell_size),
+                                     width=1)
 
 
 class Unit(pygame.sprite.Sprite):
@@ -81,10 +81,10 @@ class Unit(pygame.sprite.Sprite):
     def get_move(self):
         for key in move_map:
             if keys[key]:
-                move = move_map[key]
-                return move
+                move_last = move_map[key]
+                return move_last
 
-    def animation(self, move):
+    def animation(self, last_move):
         if self.count == 6:
             self.count = 0
         self.count += 1
@@ -108,34 +108,34 @@ class Unit(pygame.sprite.Sprite):
         #  print('\n\n')
 
         if go:
-            if move[0] == 1:
+            if last_move[0] == 1:
                 list_anim_right = [load_image('right_anim/right_shag_1.png', colorkey=colorkey),
                                    load_image('right_anim/right_shag_2.png', colorkey=colorkey)]
                 self.image = list_anim_right[self.count // 3 - 1]
                 if self.rect.right < board.width * board.cell_size:  # для того чтобы не выходил за границы
                     self.rect.x += speed
-            elif move[0] == -1:
+            elif last_move[0] == -1:
                 list_anim_left = [load_image('left_anim/left_shag_1.png', colorkey=colorkey),
                                   load_image('left_anim/left_shag_2.png', colorkey=colorkey)]
                 self.image = list_anim_left[self.count // 3 - 1]
                 if self.rect.left > 0:
                     self.rect.x -= speed
 
-            elif move[1] == 1:
+            elif last_move[1] == 1:
                 list_anim_up = [load_image('front_anim/front_shag_1.png', colorkey=colorkey),
                                 load_image('front_anim/front_shag_2.png', colorkey=colorkey)]
                 self.image = list_anim_up[self.count // 3 - 1]
                 if self.rect.bottom < board.height * board.cell_size:
                     self.rect.y += speed
 
-            elif move[1] == -1:
+            elif last_move[1] == -1:
                 list_anim_down = [load_image('back_anim/back_shag_1.png', colorkey=colorkey),
                                   load_image('back_anim/back_shag_2.png', colorkey=colorkey)]
                 self.image = list_anim_down[self.count // 3 - 1]
                 if self.rect.top > 0:
                     self.rect.y -= speed
 
-    def static_animation(self, move):
+    def static_animation(self, last_move):
         if self.count_static == 12:
             self.count_static = 0
 
@@ -145,34 +145,34 @@ class Unit(pygame.sprite.Sprite):
             else:
                 fruit.static_animation()
         self.count_static += 1
-        if move[0] == 1:
+        if last_move[0] == 1:
             list_anim_right = [load_image('right_anim/right_stoit_1.png', colorkey=colorkey),
                                load_image('right_anim/right_stoit_2.png', colorkey=colorkey)]
             self.image = list_anim_right[self.count_static // 6 - 1]
 
-        elif move[0] == -1:
+        elif last_move[0] == -1:
             list_anim_left = [load_image('left_anim/left_stoit_1.png', colorkey=colorkey),
                               load_image('left_anim/left_stoit_2.png', colorkey=colorkey)]
             self.image = list_anim_left[self.count_static // 6 - 1]
 
-        elif move[1] == 1:
+        elif last_move[1] == 1:
             list_anim_down = [load_image('front_anim/front_stoit_1.png', colorkey=colorkey),
                               load_image('front_anim/front_stoit_2.png', colorkey=colorkey)]
             self.image = list_anim_down[self.count_static // 6 - 1]
 
-        elif move[1] == -1:
+        elif last_move[1] == -1:
             list_anim_up = [load_image('back_anim/back_stoit_1.png', colorkey=colorkey),
                             load_image('back_anim/back_stoit_2.png', colorkey=colorkey)]
             self.image = list_anim_up[self.count_static // 6 - 1]
 
-    def spawn_ice_dino(self, move):
-        if move[0] == 1:
+    def spawn_ice_dino(self, last_move):
+        if last_move[0] == 1:
             self.image = load_image('right_anim/right_break_or_place_ice.png', colorkey=colorkey)
-        elif move[0] == -1:
+        elif last_move[0] == -1:
             self.image = load_image('left_anim/left_break_or_place_ice.png', colorkey=colorkey)
-        elif move[1] == 1:
+        elif last_move[1] == 1:
             self.image = load_image('front_anim/front_break_or_place_ice.png', colorkey=colorkey)
-        elif move[1] == -1:
+        elif last_move[1] == -1:
             self.image = load_image('back_anim/back_break_or_place_ice.png', colorkey=colorkey)
 
 
@@ -185,7 +185,7 @@ class Fruit(pygame.sprite.Sprite):
         self.name = name_person
         self.eat_fruct = pygame.mixer.Sound('звук поедания фрукта.mpeg')
         self.eat_fruct.set_volume(0.3)
-        self.rect.x, self.rect.y = [x * board.cell_size for x in possition(event_pos)]
+        self.rect.x, self.rect.y = [xx * board.cell_size for xx in possition(event_pos)]
 
     def static_animation(self):  # банан двигается
         if self.count == 12:
@@ -208,7 +208,7 @@ class Ice(pygame.sprite.Sprite):
         self.image = load_image(name_sprite, colorkey=(255, 255, 255))
         self.rect = self.image.get_rect()
         self.name = name_person
-        self.rect.x, self.rect.y = [x * board.cell_size for x in possition(event_pos)]
+        self.rect.x, self.rect.y = [xx * board.cell_size for xx in possition(event_pos)]
 
     def ice_animation(self):  # сделайте анимацию появления я хз как
         if self.count == 12:
@@ -234,7 +234,7 @@ if __name__ == '__main__':
     ice_sprites = pygame.sprite.Group()
     fruit_sprites = pygame.sprite.Group()
 
-    volume = 0  # значение от 0 до 1
+    volume = 0.5  # значение от 0 до 1
     pygame.mixer.music.load('Звук в уровне.mp3')  # загрузили
     pygame.mixer.music.play(-1)  # бесконечное повторение мелодии
     pygame.mixer.music.set_volume(volume)  # изменить громкость
@@ -272,10 +272,10 @@ if __name__ == '__main__':
                 = 'ice'  # тоже самое что и board.board[y][i] или board.board[i][y] в spawn_ice
             sprite_ice = Ice('ice', 'ice/ice.png', (ice_list[len(ice_list) - dlina_ice_list][1] * 68,
                                                     ice_list[len(ice_list) - dlina_ice_list][0] * 68))
-            """for ice in ice_sprites:
-                if sprite_ice.rect.colliderect(ice) and sprite_ice is not ice and ice.rect.x < sprite_hero.rect.x:  # столкновение льда
+            for ice in ice_sprites:
+                if sprite_ice.rect.colliderect(ice) and sprite_ice is not ice:  # столкновение льда
                     dlina_ice_list = 1
-                    break"""
+                    break
             dlina_ice_list -= 1
         if count != 0:
             move = None
